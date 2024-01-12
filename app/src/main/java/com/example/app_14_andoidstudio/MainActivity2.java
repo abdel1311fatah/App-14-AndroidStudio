9dc89dbc33bd840a5bec59719ed9aad388ec4890
Abdel Fatah <abdel1311fatah@gmail.com>
2024-01-12 13:41:15 +0100

    Comprobar

--- app/src/main/java/com/example/app_14_andoidstudio/MainActivity2.java
+++ app/src/main/java/com/example/app_14_andoidstudio/MainActivity2.java
@@ -13,11 +13,14 @@ import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.HashMap;
 import java.util.List;
 
+// cambiar variables i estructura
 public class MainActivity2 extends AppCompatActivity {
 
     private ConstraintLayout constraintLayout;
@@ -67,24 +70,76 @@ public class MainActivity2 extends AppCompatActivity {
     public void save(View view) {
 
         try {
-            OutputStreamWriter archivo = new OutputStreamWriter(openFileOutput("fitxer.txt", MainActivity2.MODE_PRIVATE));
-            archivo.write(et1.getText().toString());
+            OutputStreamWriter archivo = new OutputStreamWriter(openFileOutput("dades_alumne.txt", MainActivity2.MODE_PRIVATE));
+
+            // Write data to the file
+            archivo.write("Carrer: " + carrer.getText().toString() + "\n");
+            archivo.write("Numero: " + numero.getText().toString() + "\n");
+            archivo.write("Postal: " + postal.getText().toString() + "\n");
+            archivo.write("Poblacio: " + poblacio.getText().toString() + "\n");
+            archivo.write("Estudis Acabats: " + estudis_acabats.getText().toString() + "\n");
+            archivo.write("Estudis Cursant: " + estudis_cursant.getText().toString() + "\n");
+            archivo.write("Escola: " + escola.getSelectedItem().toString() + "\n");
+
+            if (treballa_si.isChecked()) {
+                archivo.write("Treballa: Si\n");
+            } else if (treballa_no.isChecked()) {
+                archivo.write("Treballa: No\n");
+            }
+
             archivo.flush();
             archivo.close();
+            Toast.makeText(this, "Datos guardados correctamente", Toast.LENGTH_SHORT).show();
+            finish();
         } catch (IOException e) {
+            e.printStackTrace();
         }
-        Toast.makeText(this, "Datos guardados correctamente", Toast.LENGTH_SHORT).show();
-        finish();
     }
-}
 
-    }
-public void recover(View view){
+    public void recover(View view) {
+        try {
+            InputStreamReader archivo = new InputStreamReader(openFileInput("dades_alumne.txt"));
+            BufferedReader br = new BufferedReader(archivo);
 
+            // Read data from the file and set values to the corresponding variables
+            carrer.setText(getValueFromLine(br.readLine()));
+            numero.setText(getValueFromLine(br.readLine()));
+            postal.setText(getValueFromLine(br.readLine()));
+            poblacio.setText(getValueFromLine(br.readLine()));
+            estudis_acabats.setText(getValueFromLine(br.readLine()));
+            estudis_cursant.setText(getValueFromLine(br.readLine()));
+
+            String selectedEscola = getValueFromLine(br.readLine());
+            int position = ((ArrayAdapter<String>) escola.getAdapter()).getPosition(selectedEscola);
+            escola.setSelection(position);
+
+            String treballaStatus = getValueFromLine(br.readLine());
+            if ("Si".equals(treballaStatus)) {
+                treballa_si.setChecked(true);
+            } else if ("No".equals(treballaStatus)) {
+                treballa_no.setChecked(true);
+            }
+
+            br.close();
+            archivo.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-public void go(View view){
-        Intent intent=new Intent(this,MainActivity.class);
+    }
+
+    private String getValueFromLine(String line) {
+        // Assuming the line format is key: value
+        String[] parts = line.split(": ");
+        if (parts.length == 2) {
+            return parts[1];
+        } else {
+            return "";
+        }
+    }
+
+    public void go(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
-        }
+    }
 
-        }
+}
